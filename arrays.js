93187c9e576525f966d6da9a5150478aee136d16
omkar <omkarabadhe99@gmail.com>
2026-05-24 20:09:55 +0530

    update array practice

---
+++ arrays.js
@@ -0,0 +1,25 @@
+let a = 10;
+ a=20
+function foo() {
+    const a = 20;
+    return a;
+//   console.log(a);
+}
+console.log(a);
+// console.log(foo());
+
+/*
+-let variable we can reassign the value but we cannot redeclare the variable
+let a = 10;
+a= 20;
+but let a = 30; // This will throw an error because we cannot redeclare a let variable
+
+- const variable we cannot reassign the value and we cannot redeclare the variable
+const a = 10;
+a= 20; // This will throw an error because we cannot reassign a const variable
+const a = 30; // This will also throw an error because we cannot redeclare a const variable
+
+ - var variable we can reassing the value and we can redclare thi variable
+var a = 10;
+var a=20
+*/
